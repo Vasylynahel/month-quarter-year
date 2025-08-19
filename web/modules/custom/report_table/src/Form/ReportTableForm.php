0e976a4de3a1a1fdca3af8a5ed9aba792b4c7a51
Vasylyna <vasilinkailcuk@gmail.com>
2025-08-19 12:34:55 +0300

    Table Form with Style

---
+++ web/modules/custom/report_table/src/Form/ReportTableForm.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Drupal\report_table\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class ReportTableForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'report_table_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $current_year = date('Y');
+
+    // Правильний порядок колонок.
+    $months = [
+      'Jan', 'Feb', 'Mar', 'Q1',
+      'Apr', 'May', 'Jun', 'Q2',
+      'Jul', 'Aug', 'Sep', 'Q3',
+      'Oct', 'Nov', 'Dec', 'Q4',
+      'YTD',
+    ];
+
+    $header = array_merge(['Year'], $months);
+
+    // Підключення CSS бібліотеки
+    $form['#attached']['library'][] = 'report_table/report_table.styles';
+    $form['#attributes']['class'][] = 'report-table-wrapper';
+
+    // Таблиця
+    $form['table'] = [
+      '#type' => 'table',
+      '#header' => $header,
+      '#attributes' => ['class' => ['report-table']],
+    ];
+
+    // Рядок поточного року
+    $form['table'][$current_year]['year'] = [
+      '#markup' => $current_year,
+    ];
+
+    // Клітинки
+    foreach ($months as $month) {
+      $cell = [
+        '#type' => 'number',
+        '#default_value' => '',
+        '#min' => 0,
+        '#attributes' => ['class' => []],
+      ];
+
+      if (in_array($month, ['Q1', 'Q2', 'Q3', 'Q4', 'YTD'])) {
+        $cell['#attributes']['class'][] = 'quarter-cell';
+      }
+
+      $form['table'][$current_year][$month] = $cell;
+    }
+
+    // Кнопка
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Save'),
+      '#button_type' => 'primary',
+      '#attributes' => ['class' => ['report-table-submit']],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $values = $form_state->getValue('table');
+    \Drupal::messenger()->addMessage('Data saved: ' . print_r($values, TRUE));
+  }
+
+}
+
